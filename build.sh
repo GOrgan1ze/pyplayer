f082a2591dc7a2006734b40c03aad37d2da73751
Sergey Surazhskyi <s.surazhskyi@gmail.com>
2016-10-07 07:09:13 -0400

    Build.sh: installation option added

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
+#!/bin/bash
+
+case $1 in
+	"-s")
+		echo 'Installing... sudo is required'
+		sudo apt-get install python3 python3-pip
+		sudo apt-get install vlc browser-plugin-vlc python3-dbus
+		sudo apt-get install python3-tk
+		pip3 install python-vlc
+		pip3 install vk
+	;;
+	*)
+		echo 'Defatult launch'
+esac
+
 killall -9 python3
 python3 main.py
